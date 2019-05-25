ae2c6d9619d4a90e9757593f099cc3db54c7241a
Hamza Beddouka <hamza.beddouka.smr@gmail.com>
2019-05-25 17:26:59 +0200

    JavaDoc

--- src/Persona.java
+++ src/Persona.java
@@ -1,11 +1,31 @@
 import java.sql.Date;
-
+/**
+ * 
+ * @author Laura Massanet Puentes
+ *
+ */
 public class Persona {
 	//atributos
+	/**
+	 * Atributo nombre define el nombre de la persona
+	 */
 	private String nombre;
+	/**
+	 * Atributo edad define la edad de la persona
+	 */
 	private int edad;
+	/**
+	 * Atributo fechaNacimiento define la fecha de nacimiento 
+	 */
 	private Date fechaNacimiento;
 	//constructor
+	/**
+	 * 
+	 * @param nombre de tipo String y define el nombre de la persona
+	 * @param edad de tipo int y define la edad de la persona
+	 * @param fechaNacimiento de tipo Date y define la fecha de nacimiento de la persona
+	 * Este es el constuctor que inicializa los atributos de la persona
+	 */
 	public Persona(String nombre, int edad, Date fechaNacimiento) {
 		super();
 		this.nombre = nombre;
@@ -13,21 +33,45 @@ public class Persona {
 		this.fechaNacimiento=fechaNacimiento;
 	}
 	//getters y setters
+	/**
+	 * 
+	 * @return nombre de tipo String
+	 */
 	public String getNombre() {
 		return nombre;
 	}
+	/**
+	 * 
+	 * @param nombre de tipo String que establece el valor del atibuto nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	/**
+	 * 
+	 * @return edad de tipo int
+	 */
 	public int getEdad() {
 		return edad;
 	}
+	/**
+	 * 
+	 * @param edad de tipo int que establece el valor del atibuto edad
+	 */
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
+	/**
+	 * 
+	 * @return fechaNacimiento de tipo Date 
+	 */
 	public Date getFechaNacimiento() {
 		return fechaNacimiento;
 	}
+	/**
+	 * 
+	 * @param fechaNacimiento de tipo Date que establece el valor del atibuto fechaNacimiento
+	 */
 	public void setFechaNacimiento(Date fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
 	}	
